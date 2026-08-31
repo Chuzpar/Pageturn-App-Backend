05c14602e35aa9b673f30e4fe1594908661838f6
Yasin <yasinhassan004@gamil.com>
2026-08-31 13:18:26 +0300

    chore: fix port run on 5000

--- run.py
+++ run.py
@@ -6,5 +6,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # --- Sprint 1 - Task 2: Configure Flask Backend ---
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
